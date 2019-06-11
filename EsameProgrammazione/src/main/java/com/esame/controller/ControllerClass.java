89bb4ecb2f46e7e36545aa13c841544a4dcfb771
Cristian Vitali <hp@DESKTOP-ETSKT4S>
2019-06-11 11:17:25 +0200

    Aggiunto il parsing della richiesta Json e la gestione dalle richiesta "GET /data"

--- EsameProgrammazione/src/main/java/com/esame/controller/ControllerClass.java
+++ EsameProgrammazione/src/main/java/com/esame/controller/ControllerClass.java
@@ -1,13 +1,19 @@
 package com.esame.controller;
 
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 
+import org.json.simple.parser.ParseException;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.esame.database.DatabaseClass;
 import com.esame.model.Metadata;
+import com.esame.model.Record;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 
 
 @RestController
@@ -17,5 +23,11 @@ public class ControllerClass {
 	public ArrayList<Metadata> getMetadata(){
 		
 		return DatabaseClass.getArrayMetadata();
+	}
+	
+	@RequestMapping(value = "data", method=RequestMethod.GET)
+	public ArrayList<Record> getDataWithGet() throws JsonParseException, JsonMappingException, IOException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, ClassNotFoundException, NoSuchMethodException, SecurityException, ParseException{
+		
+		return DatabaseClass.getRecords();
 	}
 }
