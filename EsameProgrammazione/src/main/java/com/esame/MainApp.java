426216bffa110f61b5f986b0a4d50f18429d523d
Cristian Vitali <hp@DESKTOP-ETSKT4S>
2019-06-11 10:48:58 +0200

    Aggiunto download all'avvio, creazione cartella configFile e parsing del Csv

--- EsameProgrammazione/src/main/java/com/esame/MainApp.java
+++ EsameProgrammazione/src/main/java/com/esame/MainApp.java
@@ -3,12 +3,14 @@ package com.esame;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.esame.database.DatabaseClass;
+
 
 @SpringBootApplication
 public class MainApp {
 
 		public static void main(String[] args) {
-			
+			DatabaseClass.downloadCsv("https://www.dati.gov.it/api/3/action/package_show?id=fd5e0f99-f778-4ccc-8c19-2a9fbfa59b6c");
 			SpringApplication.run(MainApp.class, args);
 		}
 }
--- EsameProgrammazione/src/main/java/com/esame/database/DatabaseClass.java
+++ EsameProgrammazione/src/main/java/com/esame/database/DatabaseClass.java
@@ -1,14 +1,38 @@
 package com.esame.database;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URI;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
+import org.json.simple.parser.ParseException;
+
 import com.esame.model.Metadata;
+import com.esame.model.Record;
+import com.esame.service.CsvParser;
+
 
 
 public class DatabaseClass {
 
+	private static CsvParser csvParser = new CsvParser();	
+	private static ArrayList<Record>  records = new ArrayList<Record>();
 	private static ArrayList<Metadata> metadata = new ArrayList<Metadata>();
 
+	
+	public static ArrayList<Record> getRecords() {
+		return records;
+	}	
 
 	public static ArrayList<Metadata> getArrayMetadata() {
 		
@@ -23,4 +47,55 @@ public class DatabaseClass {
 		metadata.add(new Metadata("TotEsRicPres","Totale esercici ricettivi - Presenze","Integer"));
 		return metadata;
 	}	
+	
+	public static void downloadCsv(String url) {
+		
+		try {
+			
+			URLConnection openConnection = new URL(url).openConnection();
+			openConnection.addRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:25.0) Gecko/20100101 Firefox/25.0");
+			InputStream in = openConnection.getInputStream();
+			
+			String data = "";
+			String line = "";
+			try {
+				InputStreamReader inR = new InputStreamReader( in );
+				BufferedReader buf = new BufferedReader( inR );
+				  
+				while ( ( line = buf.readLine() ) != null ) {
+					data+= line;
+				}
+			} finally {
+				in.close();
+			}
+			 
+			JSONObject obj = (JSONObject) JSONValue.parseWithException(data); 
+			JSONObject objI = (JSONObject) (obj.get("result"));
+			JSONArray objA = (JSONArray) (objI.get("resources"));
+			
+			for(Object o: objA){
+				if ( o instanceof JSONObject ) {
+			    	JSONObject o1 = (JSONObject)o; 
+			    	String name = (String)o1.get("name");
+			    	String urlD = (String)o1.get("url");
+			    	if(name.equals("Dati anno 2016")) {
+			        	download(urlD, "configFile/dataset.csv");
+		        	}
+			 	}
+			}
+		} catch (IOException | ParseException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	public static void download(String url, String fileName) throws Exception {
+		try (InputStream in = URI.create(url).toURL().openStream()) {
+			Files.copy(in, Paths.get(fileName), StandardCopyOption.REPLACE_EXISTING);
+			records = csvParser.csvParsing("configFile/dataset.csv");
+		}
+	}
+	
 }
